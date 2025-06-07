c701dde08e23bbcb051a96b94e4edd4292cf5fba
gcerbaro <moddexgamer@gmail.com>
2025-06-07 11:47:49 -0300

    bug fix: CS conflict

--- node/communication.py
+++ node/communication.py
@@ -3,11 +3,12 @@ from config import NODE_ID, OTHER_NODES
 from logger import logger
 from state import state
 
+
 def send_request_to_all():
     state.increment_clock()
     request_clock = state.get_clock()
     state.set_requesting_cs(True, request_clock=request_clock)
-    logger.info(f"Requesting critical section at clock {request_clock}")
+    logger.info(f"{colors.YELLOW}Requesting critical section at clock {request_clock}")
 
     for node in OTHER_NODES:
         try:
@@ -17,7 +18,7 @@ def send_request_to_all():
                 timeout=2,
             )
         except Exception as e:
-            logger.error(f"Failed to send request to {node}: {e}")
+            logger.error(f"{colors.RESET}Failed to send request to {node}: {e}")
 
 def send_reply(node):
     state.increment_clock()
@@ -27,6 +28,14 @@ def send_reply(node):
             json={"node_id": NODE_ID},
             timeout=2,
         )
-        logger.info(f"[{NODE_ID}] Sent reply to {node}")
+        logger.info(f"{colors.CYAN}[{NODE_ID}] Sent reply to {node}")
     except Exception as e:
-        logger.error(f"[{NODE_ID}] Failed to send reply to {node}: {e}")
+        logger.error(f"{colors.RESET}[{NODE_ID}] Failed to send reply to {node}: {e}")
+
+class colors:
+    YELLOW = "\033[93m"
+    CYAN = "\033[96m"
+    RESET = "\033[0m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    MAGENTA = "\033[95m"
--- node/routes.py
+++ node/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from state import state
 from config import NODE_ID
-from communication import send_reply
+from communication import send_reply, colors
 from logger import logger
 
 bp = Blueprint('routes', __name__)
@@ -17,18 +17,21 @@ def on_request():
 
     state.increment_clock(sender_clock)
 
-    logger.info(f"[{NODE_ID}] Received REQUEST from {sender_id} with clock {sender_clock}")
+    logger.info(f"{colors.CYAN}[{NODE_ID}] Received REQUEST from {sender_id} with clock {sender_clock}")
 
     defer = False
     requesting, my_clock = state.get_request_state()
     if requesting:
-        if (my_clock < sender_clock) or (my_clock == sender_clock and node_number(NODE_ID) < node_number(sender_id)):
+        if (sender_clock < state.request_clock) or (sender_clock == state.request_clock and sender_id < NODE_ID):
             defer = True
+    else:
+        send_reply(sender_id + ":5000")
+
 
 
     if defer:
         state.deferred_replies.add(sender_id)
-        logger.info(f"[{NODE_ID}] Deferred reply to {sender_id}")
+        logger.info(f"{colors.MAGENTA}[{NODE_ID}] Deferred reply to {sender_id}")
     else:
         send_reply(sender_id + ":5000")
 
@@ -42,7 +45,7 @@ def on_reply():
 
     state.increment_clock()
 
-    logger.info(f"[{NODE_ID}] Received REPLY from {sender_id}")
+    logger.info(f"{colors.CYAN}[{NODE_ID}] Received REPLY from {sender_id}")
     state.replies_received.add(sender_id)
 
     return jsonify({"ok": True})
@@ -53,7 +56,7 @@ def on_release():
     data = request.get_json()
     sender_id = data["node_id"]
 
-    logger.info(f"[{NODE_ID}] Received RELEASE from {sender_id}")
+    logger.info(f"{colors.GREEN}[{NODE_ID}] Received RELEASE from {sender_id}")
 
     if sender_id in state.deferred_replies:
         send_reply(sender_id + ":5000")
